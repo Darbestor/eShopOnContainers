65e06e248031806314a6d59dea61d1c8b31dab86
darbestor <fordarbe@gmail.com>
2023-08-30 19:25:05 +0300

    moved migration to sqlserver folder

--- src/Services/Catalog/Catalog.API/Infrastructure/CatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Infrastructure/CatalogContext.cs
@@ -18,13 +18,13 @@ public class CatalogContext : DbContext
 }
 
 
-public class CatalogContextDesignFactory : IDesignTimeDbContextFactory<CatalogContext>
-{
-    public CatalogContext CreateDbContext(string[] args)
-    {
-        var optionsBuilder = new DbContextOptionsBuilder<CatalogContext>()
-            .UseSqlServer("Server=.;Initial Catalog=Microsoft.eShopOnContainers.Services.CatalogDb;Integrated Security=true");
-
-        return new CatalogContext(optionsBuilder.Options);
-    }
-}
+// public class CatalogContextDesignFactory : IDesignTimeDbContextFactory<CatalogContext>
+// {
+//     public CatalogContext CreateDbContext(string[] args)
+//     {
+//         var optionsBuilder = new DbContextOptionsBuilder<CatalogContext>()
+//             .UseSqlServer("Server=.;Initial Catalog=Microsoft.eShopOnContainers.Services.CatalogDb;Integrated Security=true");
+//
+//         return new CatalogContext(optionsBuilder.Options);
+//     }
+// }
