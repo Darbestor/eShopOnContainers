7747d3147a7c095147bbcea4e02ac4a289ba9f65
darbestor <fordarbe@gmail.com>
2023-09-16 14:48:11 +0300

    removed protobuf dependecy

--- src/BuildingBlocks/EventBus/EventBus/Events/KafkaProtobufIntegrationEvent.cs
+++ src/BuildingBlocks/EventBus/EventBus/Events/KafkaProtobufIntegrationEvent.cs
@@ -1,16 +1,14 @@
-﻿using Google.Protobuf;
-
-namespace Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Events;
+﻿namespace Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Events;
 
 public class KafkaIntegrationEvent
 {
     public string Topic { get; private init; }
     public string Key { get; private init; }
-    public IMessage Message { get; private init; }
+    public object Message { get; private init; }
 
     public IEnumerable<KeyValuePair<string, string>> Headers { get; set; }
 
-    public KafkaIntegrationEvent(string topic, string key, IMessage message, IEnumerable<KeyValuePair<string, string>>? headers = null)
+    public KafkaIntegrationEvent(string topic, string key, object message, IEnumerable<KeyValuePair<string, string>>? headers = null)
     {
         Topic = topic ?? throw new ArgumentNullException(nameof(topic));
         Key = key;
@@ -18,7 +16,7 @@ public class KafkaIntegrationEvent
         Headers = headers ?? Array.Empty<KeyValuePair<string, string>>();
     }
     
-    public void Deconstruct(out string topic, out string key, out IMessage message,
+    public void Deconstruct(out string topic, out string key, out object message,
         out IEnumerable<KeyValuePair<string, string>> headers)
     {
         topic = Topic;
