1aab1c14c4dc9cc144c66882745d2feb6997c849
darbestor <fordarbe@gmail.com>
2023-09-18 12:05:46 +0300

    Added protobuf events for basket

---
+++ src/Services/Kafka/Protobuf/CustomTypes/GuidValueExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace Microsoft.eShopOnContainers.Services.Kafka.Protobuf;
+
+public partial class UuidValue
+{
+    public UuidValue(string guid)
+    {
+        Uuid = guid;
+    }
+
+    public static implicit operator Guid(UuidValue grpcUuid)
+    {
+        return Guid.Parse(grpcUuid.Uuid);
+    }
+
+    public static implicit operator UuidValue(Guid value)
+    {
+        return new UuidValue(value.ToString());
+    }
+}
