ba5725a222d0108c2aa7a72a52dfb9bb14146d47
darbestor <fordarbe@gmail.com>
2023-09-19 13:47:44 +0300

    cleanup

--- src/Services/Ordering/Ordering.SignalrHub/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.SignalrHub/GlobalUsings.cs
@@ -1,10 +1,7 @@
 ﻿global using Microsoft.AspNetCore.Authentication.JwtBearer;
 global using Microsoft.AspNetCore.Authorization;
 global using Microsoft.AspNetCore.SignalR;
-global using Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Abstractions;
-global using Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Events;
 global using Microsoft.eShopOnContainers.Services.Ordering.SignalrHub;
-global using Microsoft.eShopOnContainers.Services.Ordering.SignalrHub.IntegrationEvents.Events;
 global using Services.Common;
 global using KafkaFlow;
 global using Microsoft.eShopOnContainers.Kafka.Consumers;
